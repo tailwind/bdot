86e0bc846c3948e29965c6ce0674c30c0e20a332
Waylon Flinn <waylon@tailwindapp.com>
2015-06-15 11:46:43 -0500

    initial unit test

---
+++ bdot/tests/test_carray.py
@@ -0,0 +1,19 @@
+import nose
+
+import bdot
+import bcolz
+import numpy as np
+from numpy.testing import assert_array_equal
+
+def test_dot_int64():
+
+
+	matrix = np.random.random_integers(0, 12000, size=(300000, 100))
+	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
+
+	v = bcarray[0]
+
+	result = bcarray.dot(v)
+	expected = matrix.dot(v)
+
+	assert_array_equal(expected, result)
