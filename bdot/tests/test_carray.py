05f28064187c56d70d8f50c920676b81b7eb9f32
Waylon Flinn <waylon@tailwindapp.com>
2015-06-15 11:58:00 -0500

    make test run faster

--- bdot/tests/test_carray.py
+++ bdot/tests/test_carray.py
@@ -8,7 +8,7 @@ from numpy.testing import assert_array_equal
 def test_dot_int64():
 
 
-	matrix = np.random.random_integers(0, 12000, size=(300000, 100))
+	matrix = np.random.random_integers(0, 12000, size=(30000, 100))
 	bcarray = bdot.carray(matrix, chunklen=2**13, cparams=bcolz.cparams(clevel=2))
 
 	v = bcarray[0]
