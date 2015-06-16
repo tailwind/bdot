6e3f29a39144799ca4e65d17666a92e1e547ab59
Waylon Flinn <waylon@tailwindapp.com>
2015-06-16 15:19:17 -0500

    shape check

--- bdot/carray.py
+++ bdot/carray.py
@@ -7,12 +7,14 @@ import bcolz
 
 class carray(bcolz.carray):
 
-	def dot(self, vector):
+	def dot(self, matrix):
 
 		# check dtype compatibility
-		if self.dtype.type != vector.dtype.type:
-			raise ValueError("inputs must have the same dtype. Found {0} and {1}".format(self.dtype, vector.dtype))
+		if self.dtype.type != matrix.dtype.type:
+			raise ValueError("inputs must have the same dtype. Found {0} and {1}".format(self.dtype, matrix.dtype))
 
 		# check shape
+		if not self.shape[1] == matrix.shape[0]:
+			raise ValueError("inputs must have compatible shapes. Found {0} and {1}".format(self.shape, matrix.shape))
 
-		return carray_ext._dot(self, vector)
+		return carray_ext._dot(self, matrix)
