616c730adcd0e1c311f7b230521d6543c5f26b28
Waylon Flinn <waylon@tailwindapp.com>
2015-07-16 11:52:04 -0500

    fixed initial benchmarks

--- benchmarks/asv/benchmarks.py
+++ benchmarks/asv/benchmarks.py
@@ -1,6 +1,8 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import bdot
+import numpy as np
 
 class TimeSuite:
     """
@@ -30,7 +32,7 @@ class TimeSuite:
         for x in range(500):
             self.d[x] = None
 
-    def time_matrix_2_18_vector_32:
+    def time_matrix_2_18_vector_32(self):
 
         matrix = np.random.random_integers(0, 120, size=(2 ** 18, 32))
         bcarray = bdot.carray(matrix, chunklen=2**14, cparams=bdot.cparams(clevel=2))
@@ -41,25 +43,18 @@ class TimeSuite:
 
         result = bcarray.dot(v, out=output)
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
-
 
 class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def mem_matrix_2_18_vector_32(self):
+
+        matrix = np.random.random_integers(0, 120, size=(2 ** 18, 32))
+        bcarray = bdot.carray(matrix, chunklen=2**14, cparams=bdot.cparams(clevel=2))
+
+        v = bcarray[0]
+
+        output = bcarray.empty_like_dot(v)
+
+        result = bcarray.dot(v, out=output)
+
+        return result
+
